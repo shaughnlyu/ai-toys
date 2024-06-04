5bcb98c96fd77751af09587bde7a11df387ed21a
Shaughn Lyu <shaughnlyu@gmail.com>
2024-06-04 15:06:11 +0800

    feat: Add new project SemanticKernelAndVertexAIGemini

---
+++ src/SemanticKernelAndVertexAIGemini/Program.cs
@@ -0,0 +1,32 @@
+﻿using Google.Apis.Auth.OAuth2;
+using Microsoft.SemanticKernel;
+
+#pragma warning disable SKEXP0070
+Kernel kernel = Kernel.CreateBuilder()
+    .AddVertexAIGeminiChatCompletion(
+        modelId: "gemini-1.5-pro-001",
+        bearerTokenProvider: () =>
+        {
+            return GetBearerToken();
+        },
+        location: "us-central1",
+        projectId: "project-153-424805")
+    .Build();
+
+Console.WriteLine(await kernel.InvokePromptAsync("What color is the sky?"));
+Console.WriteLine();
+
+static async Task<String> GetBearerToken()
+{
+    string? credentialFile = Environment.GetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS");
+    if (string.IsNullOrEmpty(credentialFile))
+    {
+        throw new InvalidOperationException("GOOGLE_APPLICATION_CREDENTIALS environment variable is not set.");
+    }
+
+    return await GoogleCredential
+        .FromFile(credentialFile)
+        .CreateScoped(new[] { "https://www.googleapis.com/auth/cloud-platform" })
+        .UnderlyingCredential
+        .GetAccessTokenForRequestAsync();
+}
---
+++ src/SemanticKernelAndVertexAIGemini/README.md
@@ -0,0 +1,11 @@
+# SemanticKernelAndVertexAIGemini
+
+This is a simple project that uses the Semantic Kernel and Vertex AI Gemini API.
+
+## Setup Environment
+
+Set environment variables in your system:
+
+```powershell
+[System.Environment]::SetEnvironmentVariable('GOOGLE_APPLICATION_CREDENTIALS', 'path_to_your_.json_credential_file', 'User')
+```
